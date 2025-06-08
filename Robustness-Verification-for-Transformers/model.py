105c134b6e779117dadcc3d2a54d003dc052a1e3
pauljiang03 <112199352+pauljiang03@users.noreply.github.com>
2025-06-08 05:59:06 -0400

    Update model.py

--- Robustness-Verification-for-Transformers/model.py
+++ Robustness-Verification-for-Transformers/model.py
@@ -61,7 +61,8 @@ class FeedForward(nn.Module):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(dim, hidden_dim),
-            nn.ReLU(),
+            #nn.ReLU(),
+            nn.GELU(),
             nn.Dropout(dropout),
             nn.Linear(hidden_dim, dim),
             nn.Dropout(dropout)
